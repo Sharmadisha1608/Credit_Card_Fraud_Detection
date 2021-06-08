0e81f19cc8cbd8aa066b17df7d909ec1a74eba60
Sharmadisha1608 <56456928+Sharmadisha1608@users.noreply.github.com>
2021-06-09 02:26:26 +0530

    Add files via upload

---
+++ CreditCardFraudDetection.R
@@ -0,0 +1,90 @@
+install.packages("ranger")
+install.packages("caret")
+library(ranger)
+library(caret)
+library(data.table)
+creditcard_data <- read.csv("C:/Users/hp/Desktop/creditcard.csv")
+# 1 fraud 0 otherwise
+# Data Exploration
+dim(creditcard_data)
+head(creditcard_data,6)
+tail(creditcard_data,6)
+table(creditcard_data$Class) #returns count of unique values
+summary(creditcard_data$Amount)
+names(creditcard_data)
+var(creditcard_data$Amount)
+sd(creditcard_data$Amount)
+
+# Data manipulation
+head(creditcard_data)
+creditcard_data$Amount=scale(creditcard_data$Amount)
+NewData=creditcard_data[,-c(1)]  #removes first column
+head(NewData)
+
+#Data Modelling
+install.packages("caTools")
+library(caTools)
+set.seed(123)
+data_sample = sample.split(NewData$Class,SplitRatio=0.80)
+train_data = subset(NewData,data_sample==TRUE)
+test_data = subset(NewData,data_sample==FALSE)
+dim(train_data)
+dim(test_data)
+
+#Fitting Logistic Regression Model
+Logistic_Model=glm(Class~.,test_data,family=binomial())
+summary(Logistic_Model)
+plot(Logistic_Model)
+# Residual Vs Fitted : The plot is used to detect non-linearity, unequal error variances, and outliers.
+#The Residuals vs. Leverage plots helps to identify influential data points on the model.
+
+install.packages("pROC")
+library(pROC)
+lr.predict <- predict(Logistic_Model,test_data, probability = TRUE)
+auc.gbm = roc(test_data$Class, lr.predict, plot = TRUE, col = "blue")
+
+#Fitting a Decision Tree Model
+library(rpart)
+library(rpart.plot)
+decisionTree_model <- rpart(Class ~ . , creditcard_data, method = 'class')
+predicted_val <- predict(decisionTree_model, creditcard_data, type = 'class')
+probability <- predict(decisionTree_model, creditcard_data, type = 'prob')
+rpart.plot(decisionTree_model)
+
+#Artificial neural network
+install.packages("neuralnet")
+library(neuralnet)
+ANN_model =neuralnet (Class~.,train_data,linear.output=FALSE)
+plot(ANN_model)
+predANN=compute(ANN_model,test_data)
+resultANN=predANN$net.result
+resultANN=ifelse(resultANN>0.5,1,0)
+
+#Gradient Boosting
+install.packages("gbm")
+library(gbm, quietly=TRUE)
+# Get the time to train the GBM model
+system.time(
+  model_gbm <- gbm(Class ~ .
+                   , distribution = "bernoulli"
+                   , data = rbind(train_data, test_data)
+                   , n.trees = 500
+                   , interaction.depth = 3
+                   , n.minobsinnode = 100
+                   , shrinkage = 0.01
+                   , bag.fraction = 0.5
+                   , train.fraction = nrow(train_data) / (nrow(train_data) + nrow(test_data))
+  )
+)
+# Determine best iteration based on test data
+gbm.iter = gbm.perf(model_gbm, method = "test")
+
+model.influence = relative.influence(model_gbm, n.trees = gbm.iter, sort. = TRUE)
+#Plot the gbm model
+plot(model_gbm)
+
+# Plot and calculate AUC on test data
+gbm_test = predict(model_gbm, newdata = test_data, n.trees = gbm.iter)
+gbm_auc = roc(test_data$Class, gbm_test, plot = TRUE, col = "red")
+
+print(gbm_auc)
